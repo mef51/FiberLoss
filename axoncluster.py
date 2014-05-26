b086f58fe341349442cd6d2d37b2b084e244fd1a
mohammed chamma <hms.mohammed@gmail.com>
2014-05-26 16:42:55 -0400

    create and simulate a basic cluster of axon nodes. mInf is slightly off
    mInf isn't given steady-state values for the resting voltage of 0.
    This gives some really tiny bumps at the beginning as the voltage
    finds an equilibrium

--- axoncluster.py
+++ axoncluster.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pylab
+import random
 
 class AxonPositionNode:
-    """A node of Ranvier on an Axon, as modelled by Hodgkin and Huxley in 1952"""
+    """
+    A node of Ranvier on an Axon, as modelled by Hodgkin and Huxley in 1952 .
+    This class is meant for creating axons with a specific location
+    """
     def __init__(self, x, y):
         # position
         self.x = x
@@ -27,7 +32,7 @@ class AxonPositionNode:
 
         # Hodgkin-Huxley Parametahs (from the papah!)
         params = self.params = {
-            "restingVoltage"     : 0,      # V_rest (mv)
+            "restingVoltage"     : 0.00362007157577,      # V_rest (mv) # this should be zero but mInf is broken
             "Cm"                 : 1,      # uF/cm2
             "gBarNa"             : 120,    # mS/cm2
             "gBarK"              : 36,     # mS/cm2
@@ -38,7 +43,9 @@ class AxonPositionNode:
         }
 
         self.Vm    = [params["restingVoltage"]] # The axon node's membrane potential
-        self.m     = mInf(params["restingVoltage"])
+        # self.m     = mInf(params["restingVoltage"])
+        self.m     = 0.0529550196222 # manual input cuz there's a bug in mInf
+
         self.h     = hInf(params["restingVoltage"])
         self.n     = nInf(params["restingVoltage"])
 
@@ -62,34 +69,94 @@ class AxonPositionNode:
         leakageCurrent = leakageConductance * (self.Vm[lastVm] - self.params["leakagePotential"])
         self.Vm.append(self.Vm[lastVm] + (I - sodiumCurrent - potassiumCurrent - leakageCurrent) * dt / self.params["Cm"])
 
+# Current Stimulus
+stimulusCurrent = {
+    "magnitude" : 10000, # uA. the current applied at the surface
+    "x"         : 0,    # cm
+    "y"         : 10   # cm
+}
 
+def getCurrent(t, distance, current, tPulseStart=5, pulseWidth=25):
+    if tPulseStart <= t <= (tPulseStart + pulseWidth):
+        return current / (2*np.pi * distance**2) # uA/cm2. the current that reaches the axon.
+    else:
+        return 0
+
+def getDistance(x1, y1, x2, y2):
+    x = abs(x1 - x2)
+    y = abs(y1 - y2)
+    return np.sqrt(x**2 + y**2)
+
+# the nerve is a bundle of nerve fibers, or, a cluster of axons
+nerve = {
+    "numAxons" : 25,
+    "axons"    : [],
+    "radius"   : 0.2,   # cm
+    "x"        : 0,     # cm
+    "y"        : 0      # cm
+}
+
+# Create and place the axons
+for i in range(0, nerve["numAxons"]):
+    r = random.uniform(0, nerve["radius"])  # cm
+    theta = random.uniform(0, 2*np.pi)  # radians
+
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+
+    nerve["axons"].append(AxonPositionNode(x, y))
+
+### Simulayshun
 # prototypin
-class Simulation:
+class AxonClusterSimulation:
     def __init__(self, T, dt):
         self.T = T
         selft.dt = dt
         self.timeLine = np.arange(0, T+dt, dt)
 
+    def simulate(self, nerve, stimulusCurrent):
+        T = self.T
+        dt = self.dt
+        timeLine = self.timeLine
+
+        for i in range(0, len(nerve["axons"])):
+            axonPos = (nerve["axons"][i].x, nerve["axons"][i].y)  # (x, y)
+            currentPos = (stimulusCurrent["x"], stimulusCurrent["y"]) # (x, y)
+
+            distance = getDistance(axonPos[0], axonPos[1], currentPos[0], currentPos[1])
+            effectiveCurrent = getCurrent(0, distance, stimulusCurrent["magnitude"])
+
+            # step the current axon forward IN TIIIME ♪♪
+            nerve["axons"][i].step(effectiveCurrent, dt)
+
 T    = 55    # ms
 dt   = 0.025 # ms
 timeLine = np.arange(0, T+dt, dt)
 
-# Current Stimulus
-def getCurrent(t, distance, stimulusCurrent, tPulseStart=5, pulseWidth=25):
-    if tPulseStart <= t <= (tPulseStart + pulseWidth):
-        return stimulusCurrent / (2*np.pi * distance**2) # uA/cm2. the current that reaches the axon.
-    else:
-        return 0
+for t in range(1, len(timeLine)):
+    for i in range(0, len(nerve["axons"])):
+        axonPos = (nerve["axons"][i].x, nerve["axons"][i].y)  # (x, y)
+        currentPos = (stimulusCurrent["x"], stimulusCurrent["y"]) # (x, y)
 
+        distance = getDistance(axonPos[0], axonPos[1], currentPos[0], currentPos[1])
+        effectiveCurrent = getCurrent(t*dt, distance, stimulusCurrent["magnitude"])
+        nerve["axons"][i].distance = distance
+        nerve["axons"][i].current = effectiveCurrent
 
-numAxons = 10
-stimulus = 10 # uA
-# distance = 1 # cm
+        # step the current axon forward IN TIIIME ♪♪
+        nerve["axons"][i].step(effectiveCurrent, dt)
 
-# the nerve is a bundle of nerve fibers, or, a cluster of axons
-nerveDistance = 1 # cm. The distance the center of the nerve is from the stimulus
-nerveRadius = 2 # cm
+for i in range(0, len(nerve["axons"])):
+    curr = []
+    for j in range(0, len(timeLine)):
+        curr.append(getCurrent(j*dt, nerve["axons"][i].distance, stimulusCurrent["magnitude"]))
 
-# axons = []
-# for i in range(0, numAxons):
-#     axons.append(AxonNode(distance))
+    print "plotting axon #" + str(i) + "..."
+    pylab.figure()
+    pylab.plot(timeLine, nerve["axons"][i].Vm, timeLine, curr)
+    pylab.title('Axon #' + str(i) + ": Distance = " + str(nerve["axons"][i].distance) + " cm")
+    pylab.ylabel('Membrane Potential (mV)')
+    pylab.xlabel('timeLine (msec)')
+    pylab.savefig("axons/axon" + str(i) + ".jpg")
+    pylab.close()
+
